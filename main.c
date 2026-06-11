9f28bea1a77c694273876b0b430fdc69aa76e6f2
wrobepio <piotr.wrobel.stanislaw@gmail.com>
2026-06-11 21:55:25 +0200

    implementacja encodera (1)

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 // i ustawic Fast Rising Power - bity SUT[1:0] na 01  ***********
 
 
+#include <stdio.h>
 #include <avr/eeprom.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart/uart.h"
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
 
 #define UART_DEBUG
 #define BAUD0 38400
@@ -15,10 +19,12 @@
 #define WCISNIETY 0
 #define ZWOLNIONY 1
 #define KROK 1
-#define WYP_MIN 3
+#define KROK_ENC 5
+#define WYP_MIN 0
+#define WYP_MAX 255
 
 
-//definicje PINÓW
+//definicje PINï¿½W
 #define WY PD6
 #define WY_DDR DDRD
 #define WY_PORT PORTD
@@ -28,8 +34,16 @@
 #define SWITCH_PORT PORTC
 #define SWITCH_PIN PINC
 
+#define ENC_DDR DDRD
+#define ENC_PORT PORTD
+#define ENC_PIN PIND
+#define ENC_SWITCH PD4
+#define ENC_CLK PD2
+#define ENC_DT PD3
+
 char napis[5];
 uint8_t switch_mode = 0;
+volatile uint8_t wypelnienie = 0;
 
 
 
@@ -37,26 +51,85 @@ uint8_t czekaj(uint8_t naco);
 #ifdef UART_DEBUG
 static void UARTuitoa(uint16_t liczba, char *string);
 #endif
-uint16_t ADC_run(void);
+//uint16_t ADC_run(void);
 
+#ifdef UART_DEBUG
+	const char S_NL[] PROGMEM="\n\r";
+	const char S_START[] PROGMEM="Start...\n\r";
+	const char S_TPRZYC[] PROGMEM="Tryb przycisku\n\r";
+	const char S_TPOT[] PROGMEM="Tryb potencjometru/encodera\n\r";
+	const char S_WCISNIETY[] PROGMEM="wcisniety...\n\r";
+	const char S_OEEPROM[] PROGMEM="odczyt z eeprom:";
+	const char S_ZEEPROM[] PROGMEM="zapis do eeprom:";
+	const char S_POMIAR[] PROGMEM="pomiar:";
+#endif
+
+
+//INT0 interrupt 
+ISR(INT0_vect )
+{
+	if(!bit_is_clear(ENC_PIN, ENC_DT))
+	{
+		if(wypelnienie < WYP_MAX - KROK_ENC) 
+			wypelnienie += KROK_ENC;
+		else
+			wypelnienie = WYP_MAX;
+		uart0_putc('+');
+	}
+	else
+	{
+		if(wypelnienie >= KROK_ENC) 
+			wypelnienie -= KROK_ENC;
+		else
+			wypelnienie = WYP_MIN;
+		uart0_putc('-');
+	}
+	UARTuitoa(wypelnienie, napis);
+	uart0_puts(napis);
+	uart0_puts_p(S_NL);
+}
+
+//INT1 interrupt
+ISR(INT1_vect )
+{
+	if(!bit_is_clear(ENC_PIN, ENC_CLK))
+	{
+		if(wypelnienie < WYP_MAX - KROK_ENC) 
+			wypelnienie += KROK_ENC;
+		else
+			wypelnienie = WYP_MAX;
+		uart0_putc('+');
+	}
+	else
+	{
+		if(wypelnienie >= KROK_ENC) 
+			wypelnienie -= KROK_ENC;
+		else
+			wypelnienie = WYP_MIN;
+		uart0_putc('-');
+	}
+	UARTuitoa(wypelnienie, napis);
+	uart0_puts(napis);
+	uart0_puts_p(S_NL);
+}
 
 int main(void)
 {
   
-	uint16_t adc_result = 0;
-	uint8_t wypelnienie=0, tmp=0, adc_result8=0,adc_result8_prev = 0;
+	//uint16_t adc_result = 0;
+	uint8_t tmp=0; //, adc_result8=0,adc_result8_prev = 0;
 	// next four instructions. // Niepotrzebne, wylaczony fuse bit CKDIV8
     //CLKPR=(1<<CLKPCE); 
     //CLKPR=0; // 8 MHZ
 	
 	
 	//########### I/O ###########
-	//Ustawienie pinów
+	//Ustawienie pinÃ³w
     OCR0A = WYP_MIN; // Na poczatek wypelnienie min, lampka przyciemniona
     
     WY_DDR  |= (1<<WY); // jako wyjscia
     WY_PORT |=  (1<<WY); //Stan wysoki, wygaszenie lampki
-	SWITCH_DDR  &=~ (1<<SWITCH); //Ustawienie pinów klawiszy jako wejscie 
+	SWITCH_DDR  &=~ (1<<SWITCH); //Ustawienie pinÃ³w klawiszy jako wejscie 
     SWITCH_PORT |=  (1<<SWITCH); //wlaczenie rezystora podciagajacego tzw. Pull_up
 
     ////############# PWM Fast #############
@@ -66,8 +139,8 @@ int main(void)
 
 #ifdef UART_DEBUG
 	uart0_init(UART_BAUD_SELECT(BAUD0,F_CPU));
-	uart0_puts("\n\r");
-	uart0_puts("Start...\n\r");
+	uart0_puts_p(S_NL);
+	uart0_puts_p(S_START);
 #endif 
 
 	
@@ -75,15 +148,37 @@ int main(void)
 	{
 		switch_mode = 1;
 	#ifdef UART_DEBUG
-		uart0_puts("Tryb przycisku\n\r");
+		uart0_puts_p(S_TPRZYC);
 	#endif
+	} else
+	{
+	#ifdef UART_DEBUG
+		uart0_puts_p(S_TPOT);
+		/* set ENC_CLK and ENC_DT as input */	
+		cli();
+		ENC_DDR &=~ (1 << ENC_CLK);					/* PD2 and PD3 as input */
+		ENC_DDR &=~ (1 << ENC_DT);        
+		ENC_PORT |= (1 << ENC_DT)|(1 << ENC_CLK);   /* PD2 and PD3 pull-up enabled   */
+
+		EIMSK |= (1<<INT0)|(1<<INT1);				/* enable INT0 and INT1 */
+		EICRA |= (1<<ISC01)|(1<<ISC11)|(1<<ISC10); 	/* INT0 - falling edge, INT1 - reising edge */
+
+		ENC_DDR &=~ (1 << ENC_SWITCH);				/* PD4 as input */
+		ENC_PORT |= (1 << ENC_SWITCH);   			/* PD4 pull-up enabled   */
+
+
+		/* enable interrupt's */
+		sei();	
+	#endif
+	}
+
 		eeprom_busy_wait();
 		__EEGET(wypelnienie,0); //Wczytujemy z pamieci zapisane wypelnienie
 	#ifdef UART_DEBUG
 		UARTuitoa((uint16_t)wypelnienie, napis);
-		uart0_puts("odczyt z eeprom:");
+		uart0_puts_p(S_OEEPROM);
 		uart0_puts(napis);
-		uart0_puts("\n\r");
+		uart0_puts_p(S_NL);
 	#endif
 		for(tmp=WYP_MIN;tmp<wypelnienie;tmp+=KROK) // Plynnie rozjasniamy do osiagniecia zapisanego w eeprom
 		{
@@ -91,14 +186,9 @@ int main(void)
 			_delay_ms(OPOZNIENIE);
 		}
 		OCR0A=wypelnienie;
-	} else
-	{
-	#ifdef UART_DEBUG
-		uart0_puts("Tryb potencjometru\n\r");
-	#endif
-	}
 
-	while(1) //Petla glówna
+	
+	while(1) //Petla glÃ³wna
 	{
 		//_delay_ms(500);
 		//WY_PORT ^= (1 << WY);
@@ -108,7 +198,7 @@ int main(void)
 			if(!(SWITCH_PIN & (1<<SWITCH)))//jesli klawisz jest wduszony
 			{
 			#ifdef UART_DEBUG
-				uart0_puts("wcisniety...\n\r");
+				uart0_puts_p(S_WCISNIETY);
 			#endif 
 
 				_delay_ms(50); // Czekamy chwilke
@@ -140,9 +230,9 @@ int main(void)
 							_delay_ms(300);
 						#ifdef UART_DEBUG
 							UARTuitoa((uint16_t)wypelnienie, napis);
-							uart0_puts("zapis do eeprom:");
+							uart0_puts_p(S_ZEEPROM);
 							uart0_puts(napis);
-							uart0_puts("\n\r");
+							uart0_puts_p(S_NL);
 						#endif
 						}
 						OCR0A=wypelnienie;	
@@ -167,6 +257,12 @@ int main(void)
 							eeprom_busy_wait();
 							__EEPUT(0,wypelnienie);
 							_delay_ms(300);
+						#ifdef UART_DEBUG
+							UARTuitoa((uint16_t)wypelnienie, napis);
+							uart0_puts_p(S_ZEEPROM);
+							uart0_puts(napis);
+							uart0_puts_p(S_NL);
+						#endif							
 						}
 						OCR0A=wypelnienie;
 					}
@@ -174,33 +270,55 @@ int main(void)
 			}			
 		} else
 		{
-			adc_result = ADC_run();
-			adc_result8 = adc_result / 4;
-			if( adc_result8 != adc_result8_prev )
-			{
-			#ifdef UART_DEBUG
-				UARTuitoa(adc_result, napis);
-				uart0_puts("pomiar:");
-				uart0_puts(napis);
+			// adc_result = ADC_run();
+			// adc_result8 = adc_result / 4;
+			// if( adc_result8 != adc_result8_prev )
+			// {
+			// #ifdef UART_DEBUG
+			// 	UARTuitoa(adc_result, napis);
+			// 	uart0_puts("pomiar:");
+			// 	uart0_puts(napis);
 				
-				UARTuitoa((uint16_t)adc_result8, napis);
-				uart0_puts(":");
-				uart0_puts(napis);
-				uart0_puts("\n\r");
-			#endif
-				while(adc_result8 > adc_result8_prev)
+			// 	UARTuitoa((uint16_t)adc_result8, napis);
+			// 	uart0_puts(":");
+			// 	uart0_puts(napis);
+			// 	uart0_puts("\n\r");
+			// #endif
+			// 	while(adc_result8 > adc_result8_prev)
+			// 	{
+			// 		adc_result8_prev += KROK;
+			// 		OCR0A=adc_result8_prev;
+			// 		_delay_ms(8);
+			// 	}
+			// 	while(adc_result8 < adc_result8_prev)
+			// 	{
+			// 		adc_result8_prev -= KROK;
+			// 		OCR0A=adc_result8_prev;
+			// 		_delay_ms(8);
+			// 	}
+			// }			
+
+			OCR0A=wypelnienie;
+			if(!(ENC_PIN & (1<<ENC_SWITCH)))
+			{
+				uart0_puts_p(S_OEEPROM);
+				uart0_puts_p(S_NL);
+				eeprom_busy_wait();
+				__EEGET(tmp,0); // Wczytujemy poprzednie ustawienie z EEPROM
+				if(tmp!=wypelnienie) // Jesli sie zmienilo, to zapisujemy nowe ustawienie
 				{
-					adc_result8_prev += KROK;
-					OCR0A=adc_result8_prev;
-					_delay_ms(8);
-				}
-				while(adc_result8 < adc_result8_prev)
-				{
-					adc_result8_prev -= KROK;
-					OCR0A=adc_result8_prev;
-					_delay_ms(8);
-				}
-			}			
+					OCR0A=0; //Migniemy na potwierdzenie zapisu :)
+					eeprom_busy_wait();
+					__EEPUT(0,wypelnienie);
+					_delay_ms(300);
+				#ifdef UART_DEBUG
+					UARTuitoa((uint16_t)wypelnienie, napis);
+					uart0_puts_p(S_ZEEPROM);
+					uart0_puts(napis);
+					uart0_puts_p(S_NL);
+				#endif										
+			}
+			}
 		}
 	}
 }
